94eb73d956d6713273ab907eabbceb0796d8a0f0
Wai Yan Ng <nwyraymond@gmail.com>
2023-03-24 23:30:27 -0400

    Add RouteGuard component

---
+++ receiptScanner/client/components/RouteGuard.jsx
@@ -0,0 +1,32 @@
+import { isAuthenticated } from '@/lib/authenticate';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+const PUBLIC_PATHS = ['/login', '/register', '/', '/_error'];
+
+const RouteGuard = ({ children }) => {
+	const [authorized, setAuthorized] = useState(false);
+	const router = useRouter();
+
+	useEffect(() => {
+		authCheck(router.pathname);
+		router.events.on('routeChangeComplete', authCheck);
+		return () => {
+			router.events.off('routeChangeComplete', authCheck);
+		};
+	}, []);
+
+	const authCheck = (url) => {
+		const path = url.split('?')[0];
+		if (!isAuthenticated() && !PUBLIC_PATHS.includes(path)) {
+			setAuthorized(false);
+			router.push('/login');
+		} else {
+			setAuthorized(true);
+		}
+	};
+
+	return <>{authorized && children}</>;
+};
+
+export default RouteGuard;
