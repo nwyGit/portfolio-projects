efcbb22d9efdffea74128aa277b2821989ca0c14
Wai Yan Ng <nwyraymond@gmail.com>
2023-09-19 12:21:17 -0400

    Add register modal

--- homie-comb/frontend/app/layout.tsx
+++ homie-comb/frontend/app/layout.tsx
@@ -1,6 +1,8 @@
+import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
+      <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
