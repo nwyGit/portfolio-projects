36bd736fb4eeac712e8456af40d1a1379287e81c
Wai Yan Ng <nwyraymond@gmail.com>
2023-11-22 10:44:11 -0500

    Solve NextJS image optimization API problem

--- homie-comb/frontend/next.config.js
+++ homie-comb/frontend/next.config.js
@@ -1,15 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    GOOGLE_OAUTH_URL: process.env.GOOGLE_OAUTH_URL,
-    GOOGLE_OAUTH_REDIRECT_URI: process.env.GOOGLE_OAUTH_REDIRECT_URI,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GITHUB_OAUTH_URL: process.env.GITHUB_OAUTH_URL,
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-  },
-  images: {
-    domains: ["d1f5djmdoem7hf.cloudfront.net"],
-  },
+	env: {
+		GOOGLE_OAUTH_URL: process.env.GOOGLE_OAUTH_URL,
+		GOOGLE_OAUTH_REDIRECT_URI: process.env.GOOGLE_OAUTH_REDIRECT_URI,
+		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+		GITHUB_OAUTH_URL: process.env.GITHUB_OAUTH_URL,
+		GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+	},
+	images: {
+		domains: ["d1f5djmdoem7hf.cloudfront.net"],
+    // Solve NextJS image optimization API problem
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "homiecomb.s3.us-east-2.amazonaws.com",
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
