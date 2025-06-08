c4fc62c68eb070e147736c98261db953e6fa59a0
Wai Yan Ng <nwyraymond@gmail.com>
2025-06-08 23:26:30 +0800

    Refactor Layout component by removing the unused Bubbles import for improved clarity; update BlobBackground component to use a constant for velocity instead of a mutable variable, enhancing code readability and maintainability.

--- profile/components/v1/Layout.jsx
+++ profile/components/v1/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Bubbles from "./Bubbles";
 
 const Layout = ({ resumeURL, children }) => {
 	return (
