3374ef75b9554f847f3b131103c442a327f5f459
Wai Yan Ng <nwyraymond@gmail.com>
2025-07-11 02:54:17 +0800

    Update @types/react to version 18.3.23 in package.json and package-lock.json; add Google Tag Manager scripts to _document.tsx for enhanced tracking and analytics.

--- profile/pages/_document.tsx
+++ profile/pages/_document.tsx
@@ -1,9 +1,26 @@
+import React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				{/* Google Tag (gtag.js) */}
+				<script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=AW-17247820128"
+				></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', 'AW-17247820128');
+						`,
+					}}
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
